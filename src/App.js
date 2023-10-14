53d3518426bc53d8e2f802429a5f7914880e91eb
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 09:34:37 -0500

    Event Handling in fn and class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './Components/Greet'
-import Welcome from './Components/Welcome'
-import Hello from './Components/Hello'
-import Message from './Components/Message'
-import Counter from "./Components/Counter";
+import FunctionClick from './Components/FunctionClick';
+import ClassClick from './Components/ClassClick'
 function App() {
   return (
     <div className="App">
-      {/*<Greet name='Trang' heroName='Thor'>*/}
-        {/*<h1>This is the children</h1>*/}
-      {/*</Greet>*/}
-      {/*<Greet name='Thanh' heroName='Superman'><button>Action</button></Greet>
-      <Greet name='Tuan' heroName='Aquaman' />*/}
-      <Welcome heroName="Flash" />
-      {/*<Hello />*/}
-      {/* <Message/> */}
-      {/*<Counter/>*/}
+      <ClassClick />
     </div>
   );
 }
