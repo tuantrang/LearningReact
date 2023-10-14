9385db7e6134f3c2338adc422aad3966184a44d8
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 10:31:22 -0500

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionClick from './Components/FunctionClick';
 import EventBind from './Components/EventBind'
-import ParentComponent from './Components/ParentComponent'
+import UserGreeting from './Components/UserGreeting'
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <UserGreeting />
     </div>
   );
 }
