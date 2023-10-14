39bac8288a425af05c39ef02973e7371f861aa75
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 16:36:32 -0500

    Refs with Class Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import RefsDemo from './Components/RefsDemo'
+import FocusInput from './Components/FocusInput'
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo />
+      <FocusInput />
     </div>
   );
 }
