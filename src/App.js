ba5b3a6e0b5026dd4109f5a584f09ad1a53b0c53
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 14:25:21 -0500

    Basics of Form Handling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Stylesheet from './Components/Stylesheet'
-import Inline from './Components/Inline'
-import './CSS/appStyles.css'
-import styles from './CSS/appStyles.module.css'
+import Form from './Components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1>
+      <Form />
     </div>
   );
 }
