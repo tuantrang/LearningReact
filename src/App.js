2582ffae0526d5791a86e7f368dd206d70ef2681
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 20:36:19 -0500

    Higher Order Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero'
-import ErrorBoundary from './Components/ErrorBoundary'
+import HoverCounter from './Components/HoverCounter'
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Hero heroName="Clark" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
+      <HoverCounter />
     </div>
   );
 }
