096d41c45bbb109ed2d3bd9ebc49ebc5df833e93
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 21:43:26 -0500

    Render Props Part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero'
-import ClickCounter from './Components/ClickCounter'
+import ClickCounterTwo from './Components/ClickCounterTwo'
 import HoverCounter from './Components/HoverCounter'
+import HoverCounterTwo from './Components/HoverCounterTwo'
+import User from './Components/User'
+import CounterTwo from './Components/CounterTwo'
 
 function App() {
   return (
     <div className="App">
-      <ClickCounter name='Tuan' />
-      <HoverCounter name='Trang' />
+      <CounterTwo>
+        {(count, incrementCount)=> (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )} 
+      </CounterTwo>
+
+      <CounterTwo>
+        {(count, incrementCount)=> (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )}       
+      </CounterTwo>   
+      {/*<User render={(isLoggedIn)=> isLoggedIn ? 'Tuan Trang':'Guest'} />
+      <ClickCounterTwo />
+      <HoverCounterTwo />*/}
     </div>
   );
 }
