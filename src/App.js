7011fc32548595c7b6cf697037311afbc9a9e952
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 09:27:28 -0500

    Clean up files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentC from './Components/ComponentC'
-import {UserProvider} from './Components/UserContext'
+
 
 function App() {
   return (
     <div className="App">
-      {/*<UserProvider value='Tuan'>*/}
-        <ComponentC />
-      {/*</UserProvider>*/}
     </div>
   );
 }
