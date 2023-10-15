0d064ab0f3ef27bdef67a982bd87917a4bab831c
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 10:09:43 -0500

    HTTP Post Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import PostList from './Components/PostList'
+import PostForm from './Components/PostForm'
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <PostForm />
     </div>
   );
 }
