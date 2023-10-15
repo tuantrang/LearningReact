f367ecdad5be85676c49e9bdd5178bbd444bebb0
Tuan Trang <tuan.trang@gmail.com>
2023-10-14 21:00:24 -0500

    Higher Order Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/Hero'
+import ClickCounter from './Components/ClickCounter'
 import HoverCounter from './Components/HoverCounter'
 
 function App() {
   return (
     <div className="App">
+      <ClickCounter />
       <HoverCounter />
     </div>
   );
--- src/Components/ClickCounter.js
+++ src/Components/ClickCounter.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count: 0
-      }
-    }
-
-incrementCount = () => {
-    this.setState(prevState => {
-        return {count: prevState.count + 1}
-    })
-}
     
   render() {
-      const {count} = this.state;
+      const {incrementCount, count} = this.props;
     return (
       <div>
-        <button onClick={this.incrementCount}>click {count} times</button>
+        <button onClick={incrementCount}>Clicked {count} times</button>
       </div>
     )
   }
 }
 
-export default ClickCounter
+export default withCounter(ClickCounter)
