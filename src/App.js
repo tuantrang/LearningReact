54ace005ccd2b10fbbebf60bb9bfee09370df957
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 13:15:52 -0500

    useState with array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HookCounterThree from './Components/HookCounterThree'
+import HookCounterFour from './Components/HookCounterFour'
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree />
+      <HookCounterFour />
     </div>
   );
 }
