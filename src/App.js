4afc4508d64c2dc53baeeb056d4b6c326180b53d
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 19:36:29 -0500

    useContext Hook Part3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import HookCounterOne from './Components/HookCounterOne'
-import DataFetching from './Components/DataFetching'
+import ComponentC from './Components/ComponentC';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'Tuan'}>
+        <ChannelContext.Provider value={'TTTDesign'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
