7a891471f6f88f1b430c0203c21d4e790c7d5986
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 21:32:11 -0500

    Add notes

--- src/Components/Counter2.js
+++ src/Components/Counter2.js
@@ -38,3 +38,26 @@ function Counter2() {
 }
 
 export default Counter2
+
+/*
+useReducer
+userReducer is a hook that is used for state management
+It is an alternative to useState
+What's the difference?
+useState is built using useReducer
+When to useReducer vs useState
+
+useState = state
+useEffect = side effects
+userContext = context API
+userReducer = reducer
+
+reduce in Javascript
+	array.reduce(reducerFunction, initialValue)
+	singleValue = reducerFunction(accumulator, itemValue)
+	reduce method returns a single value
+useReducer in React
+	useReducer(reducerFnc, initialState)
+	newState = reducer(currentState, action)
+	useReducer returns a pair of values. [newState, dispatchMethod]
+*/
