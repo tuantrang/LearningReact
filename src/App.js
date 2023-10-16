ef72ef032ebc03b1dbfdf74a21246bceb75acaa5
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 22:02:52 -0500

    useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import logo from './logo.svg';
+import React, {useReducer, useContext} from 'react'
 import './App.css';
-import CounterThree from './Components/CounterThree';
+import ComponentA from './Components/ComponentA';
+import ComponentB from './Components/ComponentB';
+import ComponentC from './Components/ComponentC';
 
+export const CountContext = React.createContext()
 
+const initialState = 0
+const reducerFnc = (state, action) => {
+    switch(action){
+        case 'increment':
+            return state + 1
+        case 'decrement':            
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducerFnc, initialState)
   return (
-    <div className="App">
-        <CounterThree />
-    </div>
+    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+      <div className="App">
+          Count - {count}
+          <ComponentA />
+          <ComponentB />
+          <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
--- src/Components/ComponentD.js
+++ src/Components/ComponentD.js
@@ -1,11 +1,11 @@
 import React, {useContext} from 'react'
 import { CountContext } from '../App'
 
-function ComponentF() {
+function ComponentD() {
   const countContext = useContext(CountContext)
   return (
     <div>
-      Component F - {countContext.countState}
+        Component D - {countContext.countState}
       <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
       <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
       <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
@@ -13,4 +13,4 @@ function ComponentF() {
   )
 }
 
-export default ComponentF
+export default ComponentD
