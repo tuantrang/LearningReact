c344215ad638c1b81007b340b90292095fa26f72
Tuan Trang <tuan.trang@gmail.com>
2023-10-15 21:31:29 -0500

    useReducer (complex state & action)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Counter1 from './Components/Counter1';
+import Counter2 from './Components/Counter2';
 
 
 
 function App() {
   return (
     <div className="App">
-        <Counter1 />
+        <Counter2 />
     </div>
   );
 }
